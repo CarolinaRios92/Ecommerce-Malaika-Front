7247b2336d9756915231e49f7a012455b909c315
unknown <caritu_rios@hotmail.com>
2023-06-12 11:43:10 -0300

    search

--- pages/search.js
+++ pages/search.js
@@ -2,8 +2,11 @@ import Center from "@/components/Center";
 import Header from "@/components/Header";
 import styled from "styled-components";
 import Input from "@/components/Input";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
+import ProductsGrid from "@/components/ProductsGrid";
+import { debounce } from "lodash";
+import Spinner from "@/components/Spinner";
 
 const SearchInput = styled(Input)`
     padding: 5px 10px;
@@ -14,15 +17,29 @@ const SearchInput = styled(Input)`
 
 export default function SearchPage(){
     const [phrase, setPhrase] = useState("");
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const debouncedSearch = useCallback(
+        debounce(searchProducts, 500), []);
 
     useEffect(() => {
         if(phrase.length > 0){
-            axios.get("/api/products?phrase="+encodeURIComponent(phrase))
-                .then(response => {
-                    console.log(response.data)
-                })
+            setIsLoading(true);
+           debouncedSearch(phrase);
+        } else {
+            setProducts([]);
         }
     }, [phrase]);
+
+    console.log(phrase);
+
+    function searchProducts(phrase){
+         axios.get("/api/products?phrase="+encodeURIComponent(phrase))
+                .then(response => {
+                    setProducts(response.data);
+                    setIsLoading(false)
+                })
+    }
 
     return (
         <>
@@ -33,6 +50,16 @@ export default function SearchPage(){
                     onChange={e => setPhrase(e.target.value)}
                     autoFocus
                     placeholder="Buscar..."/>
+                
+                {!isLoading && phrase !== "" && products.length === 0 && (
+                    <h2>No se encontraron productos para la busqueda &quot;{phrase}&quot;</h2>
+                )}
+                {isLoading && (
+                    <Spinner fullWidth={true} />
+                )}
+                {!isLoading && products.length > 0 && phrase !== "" && (
+                    <ProductsGrid products={products} />
+                )}
             </Center>
         </>
     )
