99f418f91ec30ad4dbaeea747906c895077ffb27
unknown <caritu_rios@hotmail.com>
2023-06-13 14:01:54 -0300

    auth

--- pages/account.js
+++ pages/account.js
@@ -2,32 +2,41 @@ import Button from "@/components/Button";
 import Center from "@/components/Center";
 import Header from "@/components/Header";
 import Title from "@/components/Title";
-import {useSession, signOut} from "next-auth/react";
+import {useSession, signOut, signIn} from "next-auth/react";
 import {useRouter} from "next/router";
 
 export default function AccountPage () {
-    const session = useSession();
+    const {data:session} = useSession();
     const router = useRouter();
+    console.log(session);
 
     async function logout(){
-        await router.push("/");
         await signOut({
             callbackUrl: process.env.NEXT_PUBLIC_URL
         });
     }
 
+    async function login(){
+        await signIn("google");
+    }
+
     return (
         <>
             <Header />
-            {process.env.NEXT_PUBLIC_URL}
             <Center>
-                
                 <Title>Tu Cuenta</Title>
                 {session && (
                     <Button 
                         primary={1}
                         onClick={logout}>
                             Logout
+                    </Button>
+                )}
+                {!session && (
+                    <Button
+                        primary={1}
+                        onClick={login}>
+                        Login
                     </Button>
                 )}
                 
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,11 +4,17 @@ import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "../../../lib/mongodb"
 
 export const authOptions = {  
-    secret: process.env.SECRET,
     providers: [    
           GoogleProvider({
             clientId: process.env.GOOGLE_FRONT_ID,
-            clientSecret: process.env.GOOGLE_FRONT_SECRET
+            clientSecret: process.env.GOOGLE_FRONT_SECRET,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
+                }
+            }
         }) 
     ],
     adapter: MongoDBAdapter(clientPromise),
